fix sigma_r scaling bug and img self bilateral bug

hw1/hw1-2.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import math
 import os
 import sys
+import pickle
 
 i_prefix, g_prefix, f_prefix = 'testdata/', 'grey/', 'filter/'
 if len(sys.argv) != 2:
@@ -18,9 +19,14 @@ def gaussian_spatial(l, sigma):
 
 def gaussian_range(window, sigma):
    l = window.shape[0]
-   center = window[l // 2, l // 2]
    window = window.astype(float)
-   index = - (window - center) ** 2 / (2 * (sigma ** 2))
+   center = window[l // 2, l // 2]
+   a = (window - center)
+   if len(window.shape) == 3:
+      x = a[:, :, 0] ** 2 + a[:, :, 1] ** 2 + a[:, :, 2] ** 2
+   else:
+      x = a ** 2
+   index = - x / (2 * (sigma ** 2))
    return np.exp(index)
 
 def bilateral_filter(img, guide, sigma_s, sigma_r):
@@ -28,15 +34,18 @@ def bilateral_filter(img, guide, sigma_s, sigma_r):
    window_s = 2 * 3 * sigma_s + 1
    pad = window_s // 2
    bgr = np.pad(img, ((pad, pad), (pad, pad), (0, 0)), 'symmetric')
-   guide = np.pad(guide, ((pad, pad), (pad, pad)), 'symmetric')
+   if len(guide.shape) == 3:
+      guide = np.pad(img, ((pad, pad), (pad, pad), (0, 0)), 'symmetric')
+   else:
+      guide = np.pad(guide, ((pad, pad), (pad, pad)), 'symmetric')
    
    gs = gaussian_spatial(window_s, sigma_s)
    filtered = np.zeros((h, w, 3))
 
    for i in range(h):
       for j in range(w):
-         grey_window = guide[i : i + window_s, j : j + window_s]
-         gr = gaussian_range(grey_window, sigma_r)
+         window = guide[i : i + window_s, j : j + window_s]
+         gr = gaussian_range(window, sigma_r)
          W = np.multiply(gs, gr).flatten()
          I = bgr[i : i + window_s, j : j + window_s].reshape(-1, 3)
          x = np.average(I, weights = W, axis = 0)
@@ -49,7 +58,7 @@ def vote():
       os.makedirs(f_prefix + folder)
 
    img = cv2.imread(i_prefix + img_name + '.png') # img
-   sigma_s, sigma_r = [1, 2, 3], [0.05, 0.1, 0.2]
+   sigma_s, sigma_r = [1, 2, 3], [12.75, 25.5, 51.0]
    vote_dic = {}
    
    for i in range(3):
@@ -58,7 +67,7 @@ def vote():
          score_board = [(k, vote_dic[k]) for k in sorted(vote_dic, key = vote_dic.get, reverse = True)]
          
          print('Score_board:')
-         for k in range(30):
+         for k in range(10):
             print('{}: {}'.format(score_board[k][0], score_board[k][1]))
 
    f = open('scoreboard/' + img_name + '.txt', 'w')
@@ -67,8 +76,9 @@ def vote():
 def one_vote(sigma_s, sigma_r, vote_dic):
    dirr = g_prefix + img_name
    dic = {}
-
    folder = img_name + '/' + str(sigma_s) + '-' + str(sigma_r)
+   bf_img = bilateral_filter(img, img, sigma_s, sigma_r)
+
    if not os.path.exists(f_prefix + folder):
       os.makedirs(f_prefix + folder)
 
@@ -79,14 +89,20 @@ def one_vote(sigma_s, sigma_r, vote_dic):
          
          guide = cv2.imread(g_prefix + img_name + '/' + file_name, 0)
          filtered = bilateral_filter(img, guide, sigma_s, sigma_r)
-         delta = np.linalg.norm(img - filtered)
+         delta = np.sum(np.absolute(bf_img - filtered))
          dic[tup] = delta
          
          cv2.imwrite(f_prefix + folder + '/' + file_name, filtered)
          print('Complete filtering {} with sigma_s = {} sigma_r = {}...'.format(file_name, sigma_s, sigma_r))
          print('Saving filtered image {}'.format(file_name))
          print('Delta = {}\n'.format(delta))
-   
+
+
+   file = open(f_prefix + folder + '/' + 'dic.pickle', 'wb')
+   pickle.dump(dic, file)
+   file.close()
+   print('Save dic successfully!')
+
    arr = [(0.1, -0.1, 0), (-0.1, 0.1, 0), (0, 0.1, -0.1), (0, -0.1, 0.1), (0.1, 0, -0.1), (-0.1, 0, 0.1)]
    
    for key in dic:
@@ -96,7 +112,7 @@ def one_vote(sigma_s, sigma_r, vote_dic):
       
       is_local_min = True
       for a in arr:
-         x = (key[0] + a[0], key[1] + a[1], key[2] + a[2])
+         x = (round(key[0] + a[0], 2), round(key[1] + a[1], 2), round(key[2] + a[2], 2))
          if x in dic and dic[x] < dic[key]:
             is_local_min = False
             break
f63b7eb65c8a21ff7795c6ac8d8054c8523e49cc
byronhsu <byronhsu1230@gmail.com>
2018-10-05 11:45:06 +0800

    clean unused files

--- hw1/run.sh
+++ hw1/run.sh
@@ -1,9 +1,9 @@
+echo "Start running 2a"
+python3 hw1-1.py 2a
+python3 hw1-2.py 2a
 echo "Start running 2b"
 python3 hw1-1.py 2b
 python3 hw1-2.py 2b
 echo "Start running 2c"
 python3 hw1-1.py 2c
 python3 hw1-2.py 2c
-echo "Start running 2a"
-python3 hw1-1.py 2a
-python3 hw1-2.py 2a

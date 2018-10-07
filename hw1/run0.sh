4d56a33470b4dda0bf02ad3075d93d3d25337a2b
byronhsu <byronhsu1230@gmail.com>
2018-10-07 09:17:13 +0800

    finish running all images

---
+++ hw1/run0.sh
@@ -0,0 +1,9 @@
+echo "Start running 0a"
+python3 hw1-1.py 0a
+python3 hw1-2.py 0a
+echo "Start running 0b"
+python3 hw1-1.py 0b
+python3 hw1-2.py 0b
+echo "Start running 0c"
+python3 hw1-1.py 0c
+python3 hw1-2.py 0c
---
+++ hw1/run1.sh
@@ -0,0 +1,9 @@
+echo "Start running 1a"
+python3 hw1-1.py 1a
+python3 hw1-2.py 1a
+echo "Start running 1b"
+python3 hw1-1.py 1b
+python3 hw1-2.py 1b
+echo "Start running 1c"
+python3 hw1-1.py 1c
+python3 hw1-2.py 1c

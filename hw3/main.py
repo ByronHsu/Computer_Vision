f2eb68eff996b139dfe3116baec0a069fa93251a
byronhsu <byronhsu1230@gmail.com>
2018-11-21 00:42:45 +0800

    done part 2

--- hw3/main.py
+++ hw3/main.py
@@ -13,66 +13,99 @@ def point_to_vec(u, v):
    ]
    return vecs
 
-class Homography:
-   def __init__(self):
-      pass
-   
-   def read_file(self):
-      canvas_name = 'times_square.jpg'
-      imgs_name = ['wu.jpg', 'ding.jpg', 'yao.jpg', 'kp.jpg', 'lee.jpg']
-      canvas_corners = [
-         np.array([[352, 818], [407, 818], [352, 884], [408, 885]]),
-         np.array([[14, 311], [152, 157], [150, 402], [315, 278]]),
-         np.array([[674, 364], [864, 279], [725, 430], [885, 369]]),
-         np.array([[495, 808], [609, 802], [495, 892], [609, 896]]),
-         np.array([[608, 1024], [664, 1032], [593, 1118], [651, 1134]])
-      ]
-      imgs_corners = []
-      canvas = cv2.imread(os.path.join('input', canvas_name))
-      imgs = []
-      for i in range(5):
-         img = cv2.imread(os.path.join('input', imgs_name[i]))
-         corner = np.array([[0, 0], [img.shape[0], 0], [0, img.shape[1]], [img.shape[0], img.shape[1]]])
-         imgs.append(img)
-         imgs_corners.append(corner)
-      self.canvas, self.imgs, self.canvas_corners, self.imgs_corners = canvas, imgs, canvas_corners, imgs_corners
-   
-   def solve_homography(self, k): # 第k張圖
-      A = []
-      for i in range(4):
-         t = point_to_vec(self.imgs_corners[k][i], self.canvas_corners[k][i])
-         A.append(t[0])
-         A.append(t[1])
-      A = np.array(A)
-      u, s, vh = np.linalg.svd(A.transpose() @ A, full_matrices = False)
-      Fn = u[:, 8].reshape(3, 3)
-      H, W = self.imgs[k].shape[0], self.imgs[k].shape[1]
-      
-      pos_list = []
-      for i in range(H):
-         pos_list.append([])
-         for j in range(W):
-            pos_list[i].append([i, j, 1])
-      X = np.array(pos_list)
-      X = X.reshape(-1, 3)
+def read_file():
+   canvas_name = 'times_square.jpg'
+   imgs_name = ['wu.jpg', 'ding.jpg', 'yao.jpg', 'kp.jpg', 'lee.jpg']
+   canvas_corners = [
+      np.array([[352, 818], [407, 818], [352, 884], [408, 885]]),
+      np.array([[14, 311], [152, 157], [150, 402], [315, 278]]),
+      np.array([[674, 364], [864, 279], [725, 430], [885, 369]]),
+      np.array([[495, 808], [609, 802], [495, 892], [609, 896]]),
+      np.array([[608, 1024], [664, 1032], [593, 1118], [651, 1134]])
+   ]
+   canvas = cv2.imread(os.path.join('input', canvas_name))
+   imgs = []
+   for i in range(5):
+      img = cv2.imread(os.path.join('input', imgs_name[i]))
+      corner = np.array([[0, 0], [img.shape[0], 0], [0, img.shape[1]], [img.shape[0], img.shape[1]]])
+      imgs.append(img)
+   return canvas, imgs, canvas_corners
 
-      Y = X @ Fn.transpose()
-      Expand = (1 / Y[:, 2]).reshape(-1, 1)
-      Y = Y * Expand # 讓第三維變為1
-      Y = Y.astype(np.int_) # 取x y座標 忽略第三維
-      rows = Y[:, 0]
-      cols = Y[:, 1]
-      canvas = self.canvas
-      canvas[rows, cols] = self.imgs[k].reshape(-1, 3)
-      self.canvas = canvas
+def solve_homography(u, v):
+   A = []
+   for i in range(4):
+      t = point_to_vec(u[i], v[i])
+      A.append(t[0])
+      A.append(t[1])
+   A = np.array(A)
+   u, s, vh = np.linalg.svd(A.transpose() @ A, full_matrices = False)
+   Fn = u[:, 8].reshape(3, 3)
+   return Fn
 
+def transform(img, canvas, corners):
+   img_corners = np.array([[0, 0], [img.shape[0], 0], [0, img.shape[1]], [img.shape[0], img.shape[1]]])
+   canvas_corners = corners
+   Fn = solve_homography(img_corners, canvas_corners)
+   
+   H, W = img.shape[0], img.shape[1]
+   pos_list = []
+   for i in range(H):
+      pos_list.append([])
+      for j in range(W):
+         pos_list[i].append([i, j, 1])
+   X = np.array(pos_list).reshape(-1, 3)
+   Y = X @ Fn.transpose()
+   Expand = (1 / Y[:, 2]).reshape(-1, 1)
+   Y = Y * Expand # 讓第三維變為1
+   Y = Y.astype(np.int_)
+   # 取x y座標 忽略第三維
+   rows = Y[:, 0].reshape(H, W)
+   cols = Y[:, 1].reshape(H, W)
+   canvas[rows, cols] = img
+
+
+def apply_homography():
+   canvas, imgs, canvas_corners = read_file()
+   for i in range(len(imgs)):
+      transform(imgs[i], canvas, canvas_corners[i])
+   cv2.imwrite(os.path.join('output', 'part1.png'), canvas)
+
+def transform2(img, canvas, canvas_corners, img_corners):
+   Fn = solve_homography(img_corners, canvas_corners)
+   Fn_i = np.linalg.inv(Fn) # inverse
+
+   H, W = canvas.shape[0], canvas.shape[1]
+   pos_list = []
+   for i in range(H):
+      pos_list.append([])
+      for j in range(W):
+         pos_list[i].append([i, j, 1])
+
+   X = np.array(pos_list).reshape(-1, 3)
+   Y = X @ Fn_i.transpose()
+   Expand = (1 / Y[:, 2]).reshape(-1, 1)
+   Y = Y * Expand # 讓第三維變為1
+   Y = Y.astype(np.int_)
+
+   # 取x y座標 忽略第三維
+   rows = Y[:, 0].reshape(H, W)
+   cols = Y[:, 1].reshape(H, W)
+
+   canvas[:] = img[rows, cols]
+
+def part2():
+   canvas = np.zeros((500, 500, 3), dtype = np.int_)
+   img = cv2.imread(os.path.join('input', 'screen.jpg'))
+   img_corners = [[368, 1040], [550, 977], [400, 1100], [600, 1040]]
+   canvas_corners = [[0, 0], [500, 0], [0, 500], [500, 500]]
+   transform2(img, canvas, canvas_corners, img_corners)
+   cv2.imwrite(os.path.join('output', 'part2.png'), canvas)
 
 def main():
-   H = Homography()
-   H.read_file()
-   for i in range(5):
-      H.solve_homography(i)
-   cv2.imwrite(os.path.join('output', 'part1.png'), H.canvas)
+   # part 1
+   # apply_homography()
+   # part 2
+   part2()
 
 if __name__ == '__main__':
    main()

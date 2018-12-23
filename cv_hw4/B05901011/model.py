af987c2d4c4ed098d73af7a7f6a7267583dd92ab
byronhsu <byronhsu1230@gmail.com>
2018-12-23 23:31:25 +0800

    finish report

---
+++ cv_hw4/B05901011/model.py
@@ -0,0 +1,200 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import sys
+
+np.set_printoptions(edgeitems = 5)
+
+class BSM():
+    def __init__(self, max_disp, scale_factor):
+        self.patch_size = 26
+        self.des_len = 8096
+        self.std = 4
+        self.max_disp = max_disp
+        self.scale_factor = scale_factor
+    def setPairDistr(self):
+        # 4096 x 2
+        p_samples = np.zeros((self.des_len, 2), dtype = np.float_)
+        p_samples[:, 0] = np.random.normal(0, self.std, self.des_len).T
+        p_samples[:, 1] = np.random.normal(0, self.std, self.des_len).T
+        p_samples = np.clip(p_samples.astype(np.int_), -13, 13)
+        
+        q_samples = np.zeros((self.des_len, 2), dtype = np.float_)
+        q_samples[:, 0] = np.random.normal(0, self.std, self.des_len).T
+        q_samples[:, 1] = np.random.normal(0, self.std, self.des_len).T
+        q_samples = np.clip(q_samples.astype(np.int_), -13, 13)
+
+        self.p_samples = p_samples
+        self.q_samples = q_samples
+        print('finish setting pair distribution')
+
+    def load_image(self, l_img, r_img):
+
+        img_left_bgr, img_right_bgr = l_img, r_img
+
+        self.img_left_bgr = img_left_bgr.astype(np.int_)
+        self.img_right_bgr = img_right_bgr.astype(np.int_)
+
+        self.img_left_gray = cv2.cvtColor(img_left_bgr, cv2.COLOR_BGR2GRAY).astype(np.int_)
+        self.img_right_gray = cv2.cvtColor(img_right_bgr, cv2.COLOR_BGR2GRAY).astype(np.int_)
+        
+        self.img_left_lab = cv2.cvtColor(img_left_bgr, cv2.COLOR_BGR2LAB)[:, :, 0].astype(np.int_)
+        self.img_right_lab = cv2.cvtColor(img_right_bgr, cv2.COLOR_BGR2LAB)[:, :, 0].astype(np.int_)
+
+        self.row, self.col = self.img_left_gray.shape
+
+    def match_costing(self):
+        self._preprocess()
+        row, col = self.row, self.col
+        self.left_disparity_map = np.zeros((row, col), dtype = np.int_)
+        
+        # LEFT
+        for r in range(row):
+            for c in range(col):
+                source_bits = self.left_bstrs[r, c]
+                mask_bits = self.left_mask_bstrs[r, c]
+                ham_dis = 1e9 # initialize
+                disparity = None
+                for d in range(self.max_disp):
+                    if c - d >= 0: 
+                        target_bits = self.right_bstrs[r, c - d]
+                        count = np.count_nonzero(source_bits ^ target_bits & mask_bits)
+                        # count = np.count_nonzero(source_bits ^ target_bits)
+                        if count < ham_dis:
+                            disparity = d
+                            ham_dis = count
+                self.left_disparity_map[r, c] = disparity * self.scale_factor
+            print('matching_cost left', r)
+            # cv2.imwrite('output/{}-left.png'.format(NAME), self.left_disparity_map)
+        # RIGHT
+        self.right_disparity_map = np.zeros((row, col), dtype = np.int_)
+        for r in range(row):
+            for c in range(col):
+                source_bits = self.right_bstrs[r, c]
+                mask_bits = self.right_mask_bstrs[r, c]
+                ham_dis = 1e9 # initialize
+                disparity = None
+                for d in range(self.max_disp):
+                    if c + d < col: 
+                        target_bits = self.left_bstrs[r, c + d]
+                        count = np.count_nonzero(source_bits ^ target_bits & mask_bits)
+                        # count = np.count_nonzero(source_bits ^ target_bits)
+                        if count < ham_dis:
+                            disparity = d
+                            ham_dis = count
+                self.right_disparity_map[r, c] = disparity * self.scale_factor
+            print('matching_cost right', r)
+            # cv2.imwrite('output/{}-right.png'.format(NAME), self.right_disparity_map)
+        
+    def skip_cost(self):
+        self.left_disparity_map = cv2.imread('output/{}-left.png'.format(NAME), 0)
+        self.right_disparity_map = cv2.imread('output/{}-right.png'.format(NAME), 0)
+
+    def refine_median(self):
+        left_disp = self.left_disparity_map.astype(np.uint8)
+        kernal_size = 7
+        left_disp = cv2.medianBlur(left_disp, kernal_size)
+        left_disp = cv2.bilateralFilter(left_disp, 10, 9, 16)
+        self.left_disparity_map = left_disp
+        # cv2.imwrite('{}.png'.format(NAME), left_disp)
+
+    def refine_BSM(self):
+        row, col = self.row, self.col
+        valid_map = np.zeros((row, col), dtype = np.bool_)
+        left_disp = self.left_disparity_map.astype(np.int_) // self.scale_factor
+        right_disp = self.right_disparity_map.astype(np.int_) // self.scale_factor
+
+        new_disp = left_disp.copy()
+        for r in range(row):
+            for c in range(col):
+                match_r, match_c = (r, c - left_disp[r, c])        
+                if match_c < 0:
+                    valid_map[r, c] = False
+                else:
+                    r_disp = right_disp[match_r, match_c]
+                    l_disp = left_disp[r, c]
+                    if abs(r_disp - l_disp) > 1:
+                        valid_map[r, c] = False
+                    else:
+                        valid_map[r, c] = True
+
+        temp_map = valid_map.copy().astype(np.int_) * 255
+        # cv2.imwrite('valid_map.png', temp_map)
+
+        r_axis = np.tile(range(col), (row, 1))
+        c_axis = np.tile(range(row), (col, 1)).T
+
+        LDc, LDe = 9, 16
+        refine_threshold = self.max_disp // 6
+        for r in range(row):
+            for c in range(col):
+                if valid_map[r, c] == False:
+                    disparity = 1e5
+                    for k in range(col):
+                        if c - k < 0:
+                            break
+                        elif valid_map[r, c - k] == True and left_disp[r, c - k] > refine_threshold:
+                            disparity = min(disparity, left_disp[r, c - k])
+                            break
+                    for k in range(col):
+                        if c + k >= col:
+                            break
+                        elif valid_map[r, c + k] == True and left_disp[r, c + k] > refine_threshold:
+                            disparity = min(disparity, left_disp[r, c + k])
+                            break
+                    new_disp[r, c] = disparity
+            # cv2.imwrite('output/{}.png'.format(NAME), new_disp * self.scale_factor)
+            # print(r)
+        self.left_disparity_map = new_disp * self.scale_factor
+                    
+
+    def _preprocess(self):
+        row, col = self.row, self.col
+        left_bstrs = np.zeros((row, col, self.des_len), dtype = np.bool_)
+        right_bstrs = np.zeros((row, col, self.des_len), dtype = np.bool_)
+        left_mask_bstrs = np.zeros((row, col, self.des_len), dtype = np.bool_)
+        right_mask_bstrs = np.zeros((row, col, self.des_len), dtype = np.bool_)
+
+        img_left_pad = np.pad(self.img_left_gray, (self.patch_size // 2, self.patch_size // 2), 'constant', constant_values=(0, 0))
+        img_right_pad = np.pad(self.img_right_gray, (self.patch_size // 2, self.patch_size // 2), 'constant', constant_values=(0, 0))
+        
+        img_left_lab_pad = np.pad(self.img_left_lab, (self.patch_size // 2, self.patch_size // 2), 'constant', constant_values=(0, 0))
+        img_right_lab_pad = np.pad(self.img_right_lab, (self.patch_size // 2, self.patch_size // 2), 'constant', constant_values=(0, 0))
+        
+        for r in range(row):
+            for c in range(col):
+                left_bstrs[r, c] = self._findbinstring(r, c, img_left_pad)
+                right_bstrs[r, c] = self._findbinstring(r, c, img_right_pad)
+                left_mask_bstrs[r, c] = self._findmask(r, c, img_left_lab_pad)
+                right_mask_bstrs[r, c] = self._findmask(r, c, img_right_lab_pad)
+            print('_preprocess', r)
+        self.left_bstrs = left_bstrs
+        self.right_bstrs = right_bstrs
+        self.left_mask_bstrs = left_mask_bstrs
+        self.right_mask_bstrs = right_mask_bstrs
+
+    def _findbinstring(self, r_offset, c_offset, image_pad):
+        pad = self.patch_size // 2
+        p_r = self.p_samples[:, 0] + r_offset + pad
+        p_c = self.p_samples[:, 1] + c_offset + pad
+        q_r = self.q_samples[:, 0] + r_offset + pad
+        q_c = self.q_samples[:, 1] + c_offset + pad
+        bits = image_pad[p_r, p_c] > image_pad[q_r, q_c]
+        return bits
+
+    def _findmask(self, r_offset, c_offset, image_pad):
+        pad = self.patch_size // 2
+        p_r = self.p_samples[:, 0] + r_offset + pad
+        p_c = self.p_samples[:, 1] + c_offset + pad
+        q_r = self.q_samples[:, 0] + r_offset + pad
+        q_c = self.q_samples[:, 1] + c_offset + pad
+        now_val = image_pad[r_offset + pad, c_offset + pad]
+        p_val = image_pad[p_r, p_c]
+        q_val = image_pad[q_r, q_c]
+        sad_xp = np.absolute(p_val - now_val)
+        sad_xq = np.absolute(q_val - now_val)
+        w = np.maximum(sad_xp, sad_xq)
+        w_sort = np.sort(w)
+        quarter_smallest = w_sort[len(w_sort) // 4]
+        bits = (w <= quarter_smallest)
+        return bits
--- cv_hw4/readme.md
+++ cv_hw4/readme.md
@@ -1,17 +1,62 @@
-# Stereo Matching
-| Image   |                Result                 | Bad Pixel Ratio |
-| ------- | :-----------------------------------: | --------------- |
-| Tsukuba | <img src="tsukuba.png" width="300px"> | 4.75%           |
-| Venus   |  <img src="venus.png" width="300px">  | 0.85%           |
-| Teddy   |  <img src="teddy.png" width="300px">  | 8.59%           |
-| Cones   |  <img src="cones.png" width="300px">  | 7.65%           |
-| Average |                                       | 5.46%           |
+# Computer Vision Hw4
 
-```
-[Bad Pixel Ratio]
-Tsukuba: 4.75%
-Venus: 0.85%
-Teddy: 8.59%
-Cones: 7.65%
-Average: 5.46%
-```
+> B05901011 電機三 許秉倫
+
+### Part 1: Depth from Disparity
+
+<img src="assets/p1.jpg" width="500px">
+
+### Part 2: Disparity Estimation
+
+>  **備註**: 由於我在refine時會用到disparity的灰階圖，因此我有將scale傳入``computeDisp``中
+
+#### Explain your algorithm in terms of the standard 4-step pipeline.
+
+我參考的paper是**Binary Stereo Matching**(Kang Zhang et)
+
+1. cost computation:
+
+   他的概念類似於上課提到的census，但更加強大，首先，他用高斯分佈在一個給定的方形內(邊長 S)，生成K對座標點的``pairs = (pi, qi)``，接著對每對pair做**intensity**的大小比較，若``I(pi) > I(qi)``則為1，反之則為0，經過一番運算，每個點可轉換成一個K bits的布林陣列。
+
+   對左圖和右圖都做完上述運算後，可以開始進行cost computation。
+
+   對左圖的一個點O，我們將其和右圖可能的matching point做xor的運算，算完後數其中共有幾個1，1越少代表越有可能是matching point。
+   $$
+   C(x, d) = ||B(x)\ XOR \ B(x_d)||_1
+   $$
+   此步驟做完會有**edge flatten**的問題，亦即邊界切得不夠明確。
+
+2. cost aggregation:
+
+   為了解決edge flatten的問題，我們多加了一項類似regularization的東西
+   $$
+   w(x, pi, qi) = max(SAD(x, pi), SAD(x, qi))
+   $$
+   他的用意是希望兩個matching的點顏色盡量不要差太多，並且定一個threshold T，假如w > T則為1，反之則為0，最終亦可得到一個K bits的布林陣列，matching cost變為如下
+   $$
+   C(x, d) = ||B(x)\ XOR \ B(x_d) \ AND \ \phi(x) ||_1
+   $$
+
+3. Disp optimization
+
+   我採用簡單的WTA算法
+
+4. Disp refinement
+
+   1. 先利用left-right consistency，找出invalid的點，對那些點，往左往右走，各找第一個碰到的valid點，然後選兩者中disp較小的補上去。
+   2. 再把圖片通過median filter降噪
+
+#### Show your output disparity maps and show your bad pixel ratio. 
+
+| Tsukuba                               | Venus                               | Teddy                               | Cones                               |
+| ------------------------------------- | ----------------------------------- | ----------------------------------- | ----------------------------------- |
+| <img src="tsukuba.png" width="250px"> | <img src="venus.png" width="250px"> | <img src="teddy.png" width="250px"> | <img src="cones.png" width="250px"> |
+| 4.88%                                 | 0.78%                               | 8.81%                               | 7.68%                               |
+
+**Avg score: 5.54%**
+
+#### Your reference papers or websites. 
+
+https://github.com/rookiepig/BinaryStereo
+
+https://arxiv.org/pdf/1402.2020.pdf
